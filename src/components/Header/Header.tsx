989f41c1e6baf69d7208f7f0941ede56566dd39f
Matthew Lin <matthewlinplusprogramming@gmail.com>
2023-06-15 21:00:06 -0700

    ♻️ (refactor): Refactor Header
    Add block element that sits behind Header to fix layout issues
    Add proper props passing

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,35 @@
-import classname from 'classnames';
-import React, { HTMLProps } from 'react';
-import { header, logo, logoContainer, logoText } from './Header.module.scss';
+import classnames from 'classnames';
+import React, { DetailedHTMLProps, HTMLAttributes } from 'react';
+
+// Style imports
+import {
+  header,
+  logo,
+  logoContainer,
+  logoText,
+  headerBlock
+} from './Header.module.scss';
 import Logo from '../../assets/ryde.png';
 
-type HeaderProps = HTMLProps<HTMLDivElement>
+type HeaderProps = DetailedHTMLProps<
+  HTMLAttributes<HTMLDivElement>, HTMLDivElement
+>
 
 const Header: React.FC<HeaderProps> = ({
-  className
+  className,
+  ...props
 }) => (
-  <div className={classname(header, className)}>
-    <div className={logoContainer}>
-      <img src={Logo} alt="Ryde Logo" className={classname(logo)} />
-      <span className={logoText}>Ryde</span>
-    </div>
-    {/* I thought to include dummy links, but I realized its far outside the
+  <>
+    <div className={classnames(headerBlock)} />
+    <div className={classnames(header, className)} {...props}>
+      <div className={logoContainer}>
+        <img src={Logo} alt="Ryde Logo" className={classnames(logo)} />
+        <span className={logoText}>Ryde</span>
+      </div>
+      {/* I thought to include dummy links, but I realized its far outside the
     scope of this project */}
-  </div>
+    </div>
+  </>
 );
 
 export { Header };
